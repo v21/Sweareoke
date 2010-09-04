305ebacff140258cb8fcfea88b24c0668638464d
Joe <mrjoebain@gmail.com>
2010-09-05 00:43:16 +0100

    fucking yeah boy

--- game.py
+++ game.py
@@ -1,19 +1,79 @@
 #!/usr/bin/python
 import gfx
 from gfx import *
+import word
+from word import *
 
 class Game:
 	def __init__(self):
-		self.display = Display()
+		self.run = True
+		self.guitar = None
+
+		self.level = 0
+		self.time_window = 5000
+		self.song_start_time = 0
+		self.all_words = [
+				Word(1000, "One") ,
+				Word(2000, "Two")]
+
+		self.display = Display(self.time_window)
+
+
+		self.red = 1
+		self.green = 5
+		self.yellow = 0
+		self.blue = 2
+		self.orange = 3
+		self.tip = 4
+		self.start = 9
+		self.select = 8
+
+		self.sound_button_map = {
+			self.green : 0,
+			self.red : 1,
+			self.yellow : 2,
+			self.blue : 3,
+			self.orange : 4
+		}
+
+		self.buttons = [self.green, self.red, self.yellow, self.blue, self.orange]
 
 	def init(self):
 		self.display.init()
-		self.display.title()
+		self.display.load_title()
+		self.guitar = pygame.joystick.Joystick(0)
+		self.guitar.init()
 
-	
+
+	def start_song(self):
+		self.song_start_time = pygame.time.get_ticks()
+		self.display.load_main(self.all_words)
+
+	def process_input(self):
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				self.run = False
+			elif event.type == KEYDOWN:
+				if event.key == K_ESCAPE:
+					self.run = False
+			elif event.type == JOYBUTTONDOWN:
+				if self.level == 0:
+					self.level = 1
+					self.start_song()
+			elif event.type == JOYAXISMOTION:
+				if event.value != 0:
+					for button in self.buttons:
+						if self.guitar.get_button(self.sound_button_map[button]):
+							print "BUTTON " + str(button)
+				
+
 	def main_loop(self):
-		while (True):
+		while (self.run):
+			self.process_input()
+			time = pygame.time.get_ticks() - self.song_start_time
+			self.display.update(time)
 			self.display.draw()
+			pygame.time.wait(33)
 
 
 game = Game()
--- gfx.py
+++ gfx.py
@@ -4,26 +4,30 @@ from pygame.locals import *
 
 class Display:
 
-	def __init__(self):
+	def __init__(self, time_window):
 		self.blitables = []
+		self.words = []
 		self.screen = None
-		self.bg_colour = (255,0,255)
+		self.bg_colour = (0,255,255)
 		self.time = 0
+		self.time_window = time_window
+		self.time_text = None
 
 	def init(self):
 		pygame.init()
 		self.screen = pygame.display.set_mode((640, 480))
 		pygame.display.set_caption('Sweareoke!!')
+		self.big_font = pygame.font.Font(None, 100)
+		self.small_font = pygame.font.Font(None, 60)
 
-	def title(self):
+	def load_title(self):
 		# Fill background
 		background = pygame.Surface(self.screen.get_size())
 		background = background.convert()
 		background.fill((255, 0, 255))
 
 		# Display some text
-		font = pygame.font.Font(None, 36)
-		text = font.render("SWEAREOKE", 1, (255, 255, 255))
+		text = self.big_font.render("SWEAREOKE", 1, (255, 255, 255))
 		textpos = text.get_rect()
 		textpos.centerx = background.get_rect().centerx
 		textpos.centery = background.get_rect().centery
@@ -31,21 +35,51 @@ class Display:
 
 		self.blitables.append(background)
 	
-	def main(self, notes):
+	def load_main(self, words):
+		self.clear()
+
 		# Fill background
 		background = pygame.Surface(self.screen.get_size())
 		background = background.convert()
-		background.fill(bg_colour)
-		
-		notes.time
+		background.fill(self.bg_colour)
 
-	def update(self, time)
+		self.blitables.append(background)
+		
+		self.words = words
+		for word in words:
+			word.surface = self.small_font.render(word.text, 1, (0,0,0))
+			word.pos = word.surface.get_rect()
+			word.pos.centerx = self.screen.get_width()/2
+
+		self.time_text = self.big_font.render("0", 1, (255,255,255))
+	
+	def clear(self):
+		self.blitables = []
+		self.words = []
+
+	def update(self, time):
 		self.time = time
+		for word in self.words:
+			if word.time >= time or word.time < time + self.time_window:
+				word.pos.centery = self.screen.get_height() - float(self.screen.get_height())/float(self.time_window) * float((word.time-time))
+				#print "word: " + word.text + " " + str(word.time) + " " + str(word.pos.centery)
+				word.on_screen = True
+			else:
+				word.on_screen = False
+
+		self.time_text = self.big_font.render(str(time), 1, (0, 0, 0))
 
 	def draw(self):
 		# Blit everything to the screen
 		for blitable in self.blitables:
 			self.screen.blit(blitable, (0, 0))
+		for word in self.words:
+			if word.on_screen:
+				#print "drawing word " + word.text + " at " + str(word.pos.left) + ", " + str(word.pos.top)
+				self.screen.blit(word.surface, word.pos)
+
+		self.screen.blit(self.time_text, (0,0))
+			
 
 		pygame.display.flip()
 
