92858b408365cbccf4998a028520530413f38a76
Joe <mrjoebain@gmail.com>
2010-09-05 00:30:41 +0100

    some stuff. words

---
+++ word.py
@@ -0,0 +1,11 @@
+import pygame
+from pygame import *
+
+class Word:
+
+	def __init__(self, time, text):
+		self.time = time
+		self.text = text
+		self.surface = None
+		self.on_screen = False
+		self.pos = Rect(0,0,1,1)
