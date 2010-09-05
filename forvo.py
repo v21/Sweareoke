2259e05ea6697a171aa84ffbe6285c0169e14148
George Buckenham <v21@kubrick-karmic.(none)>
2010-09-05 06:09:17 +0100

    shiny and polished

--- forvo.py
+++ forvo.py
@@ -1,28 +1,30 @@
 import urllib
 import json
-
-
+import re
+import os.path
 
 
 class ForvoResponse:
     def __init__(self, jsonResp):
         self.num_recordings = jsonResp[u"attributes"][u"total"]
 
-        self.recordings = [resp[u"pathogg"] for resp in jsonResp[u"items"]]
+        self.recordings = [{"ogg":resp[u"pathogg"], "id": resp[u"id"]} for resp in jsonResp[u"items"]]
+        self.json = jsonResp
+
 
 class ForvoLibrary:
     def __init__(self):
         self.api_key = "32afdf084bda652565de09c55de855e3"
         self.cache = {} # {"word":ForvoResponse(), ...}
-
+        self.recording_loc = "sounds"
 
     def queryWord(self, word):
 
-        if self.cache.has_key(word):
+        if self.cache.has_key(word): #links will stop working after 2 hours. this aint a problem now.
             return self.cache[word]
 
         url_start = "http://apifree.forvo.com/action/word-pronunciations/format/json/word/"
-        url_end = "/language/en/key/"
+        url_end = "/language/en/order/rate-desc/key/"
 
         url = url_start + word + url_end + self.api_key
         f = urllib.urlopen(url)
@@ -31,3 +33,31 @@ class ForvoLibrary:
         self.cache[word] = resp
         return resp
 
+    def fetchRecording(self, resp, which):
+        if (resp.num_recordings == 0) :
+            raise NoRecordingsError
+        url = resp.recordings[which]["ogg"]
+        filename = self.recording_loc + "/" + resp.recordings[which]["id"] + ".ogg"
+        if os.path.exists(filename):
+            return filename
+        else:
+            urllib.urlretrieve(url, filename)
+            return filename
+
+    def queryAndFetchMultiple(self, words):
+        words = list(set(words)) #uniquify
+        default_which = 0
+        filenames = {}
+        for word in words:
+            resp = self.queryWord(word)
+            try:
+                filename = self.fetchRecording(resp,default_which)
+                filenames[word] = filename
+            except NoRecordingsError:
+                print "Couldn't find: " + word
+
+        return filenames
+
+
+class NoRecordingsError(Exception):
+    pass
